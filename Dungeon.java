85f9ac565379753e600655b9143926c6add24060
jbrown626 <36527497+jbrown626@users.noreply.github.com>
2020-03-03 10:42:24 -0800

    Add files via upload
    deleted all the coments

--- Dungeon.java
+++ Dungeon.java
@@ -1,48 +1,4 @@
-/**
- * Title: Dungeon.java
- *
- * Description: Driver file for Heroes and Monsters project
- *
- * Copyright:    Copyright (c) 2001
- * Company: Code Dogs Inc.
- * I.M. Knurdy
- *
- * History:
- *  11/4/2001: Wrote program
- *    --created DungeonCharacter class
- *    --created Hero class
- *    --created Monster class
- *    --had Hero battle Monster
- *    --fixed attack quirks (dead monster can no longer attack)
- *    --made Hero and Monster abstract
- *    --created Warrior
- *    --created Ogre
- *    --made Warrior and Ogre battle
- *    --added battleChoices to Hero
- *    --added special skill to Warrior
- *    --made Warrior and Ogre battle
- *    --created Sorceress
- *    --created Thief
- *    --created Skeleton
- *    --created Gremlin
- *    --added game play features to Dungeon.java (this file)
- *  11/27/2001: Finished documenting program
- * version 1.0
- */
 
-
-
-/*
-  This class is the driver file for the Heroes and Monsters project.  It will
-  do the following:
-
-  1.  Allow the user to choose a hero
-  2.  Randomly select a monster
-  3.  Allow the hero to battle the monster
-
-  Once a battle concludes, the user has the option of repeating the above
-
-*/
 public class Dungeon
 {
     public static void main(String[] args)
@@ -61,11 +17,6 @@ public class Dungeon
 
     }//end main method
 
-/*-------------------------------------------------------------------
-chooseHero allows the user to select a hero, creates that hero, and
-returns it.  It utilizes a polymorphic reference (Hero) to accomplish
-this task
----------------------------------------------------------------------*/
 	public static Hero chooseHero()
 	{
 		int choice;
@@ -90,10 +41,6 @@ this task
 		}//end switch
 	}//end chooseHero method
 
-/*-------------------------------------------------------------------
-generateMonster randomly selects a Monster and returns it.  It utilizes
-a polymorphic reference (Monster) to accomplish this task.
----------------------------------------------------------------------*/
 	public static Monster generateMonster()
 	{
 		int choice;
@@ -113,10 +60,6 @@ a polymorphic reference (Monster) to accomplish this task.
 		}//end switch
 	}//end generateMonster method
 
-/*-------------------------------------------------------------------
-playAgain allows gets choice from user to play another game.  It returns
-true if the user chooses to continue, false otherwise.
----------------------------------------------------------------------*/
 	public static boolean playAgain()
 	{
 		char again;
@@ -127,13 +70,6 @@ true if the user chooses to continue, false otherwise.
 		return (again == 'Y' || again == 'y');
 	}//end playAgain method
 
-
-/*-------------------------------------------------------------------
-battle is the actual combat portion of the game.  It requires a Hero
-and a Monster to be passed in.  Battle occurs in rounds.  The Hero
-goes first, then the Monster.  At the conclusion of each round, the
-user has the option of quitting.
----------------------------------------------------------------------*/
 	public static void battle(Hero theHero, Monster theMonster)
 	{
 		char pause = 'p';
