6aa6e11d0f8c6356ef5611ecf870fe0524adb32c
achernega <61473666+achernega@users.noreply.github.com>
2020-03-04 18:41:49 -0800

    Add files via upload

--- Dungeon.java
+++ Dungeon.java
@@ -4,59 +4,13 @@ public class Dungeon
 {
     public static void main(String[] args)
 	{
-		Hero theHero;
-		Monster theMonster;
 		do
 		{
-		    theHero = chooseHero();
-		    theMonster = spawnMonster();
-			battle(theHero, theMonster);
-
+			GameSetup game = new GameSetup();
+			game.battle();
 		} while (playAgain());
     }//end main method
 
-    public static Hero chooseHero() {
-		int choice;
-		HeroFactory hf = new HeroFactory();
-		Scanner kb = new Scanner(System.in);
-		
-		System.out.println("Choose a hero:\n" +
-					       "1. Warrior\n" +
-						   "2. Sorceress\n" +
-						   "3. Thief");
-		choice = kb.nextInt();
-
-		switch(choice)
-		{
-			case 1: return hf.createHero("Warrior");
-
-			case 2: return hf.createHero("Sorceress");
-
-			case 3: return hf.createHero("Thief");
-
-			default: System.out.println("invalid choice, returning Thief");
-				return new Thief();
-		}
-	}
-    
-    public static Monster spawnMonster()
-    {
-    	MonsterFactory mf = new MonsterFactory();
-		int choice = (int)(Math.random() * 3) + 1;
-		
-		switch(choice)
-		{
-			case 1: return mf.createMonster("Ogre");
-
-			case 2: return mf.createMonster("Gremlin");
-
-			case 3: return mf.createMonster("Skeleton");
-
-			default: System.out.println("invalid choice, returning Skeleton");
-				return mf.createMonster("Skeleton");
-		}
-	}
-    
 	public static boolean playAgain()
 	{
 		char again;
@@ -67,43 +21,5 @@ public class Dungeon
 
 		return (again == 'Y' || again == 'y');
 	}//end playAgain method
-
-
-/*-------------------------------------------------------------------
-battle is the actual combat portion of the game.  It requires a Hero
-and a Monster to be passed in.  Battle occurs in rounds.  The Hero
-goes first, then the Monster.  At the conclusion of each round, the
-user has the option of quitting.
----------------------------------------------------------------------*/
-	public static void battle(Hero theHero, Monster theMonster)
-	{
-		char pause = 'p';
-		System.out.println(theHero.getName() + " battles "
-				+ theMonster.getName()
-				+ "\n---------------------------------------------");
-
-		//do battle
-		while (theHero.isAlive() && theMonster.isAlive() && pause != 'q')
-		{
-		    //hero goes first
-			theHero.battleChoices(theMonster);
-
-			//monster's turn (provided it's still alive!)
-			if (theMonster.isAlive())
-			    theMonster.attack(theHero);
-
-			//let the player bail out if desired
-			System.out.print("\n-->q to quit, anything else to continue: ");
-			Scanner kb = new Scanner(System.in);
-			pause = kb.next().charAt(0);
-
-		}//end battle loop
-
-		if (!theMonster.isAlive())
-		    System.out.println(theHero.getName() + " was victorious!");
-		else if (!theHero.isAlive())
-			System.out.println(theHero.getName() + " was defeated :-(");
-		else//both are alive so user quit the game
-			System.out.println("Quitters never win ;-)");
-	}//end battle method
+	
 }//end Dungeon class
---
+++ GameSetup.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class GameSetup
+{
+	private Hero hero;
+	private Monster monster;
+
+	public GameSetup()
+	{
+		hero = chooseHero();
+		monster = spawnMonster();
+	}
+
+	public static Hero chooseHero() {
+		int choice;
+		HeroFactory hf = new HeroFactory();
+		Scanner kb = new Scanner(System.in);
+
+		System.out.println("Choose a hero:\n" +
+				"1. Warrior\n" +
+				"2. Sorceress\n" +
+				"3. Thief");
+		choice = kb.nextInt();
+
+		switch(choice)
+		{
+		case 1: return hf.createHero("Warrior");
+
+		case 2: return hf.createHero("Sorceress");
+
+		case 3: return hf.createHero("Thief");
+
+		default: System.out.println("invalid choice, returning Thief");
+		return new Thief();
+		}
+	}
+
+	public static Monster spawnMonster()
+	{
+		MonsterFactory mf = new MonsterFactory();
+		int choice = (int)(Math.random() * 3) + 1;
+
+		switch(choice)
+		{
+		case 1: return mf.createMonster("Ogre");
+
+		case 2: return mf.createMonster("Gremlin");
+
+		case 3: return mf.createMonster("Skeleton");
+
+		default: System.out.println("invalid choice, returning Skeleton");
+		return mf.createMonster("Skeleton");
+		}
+	}
+
+	/*-------------------------------------------------------------------
+    battle is the actual combat portion of the game.  It requires a Hero
+    and a Monster to be passed in.  Battle occurs in rounds.  The Hero
+    goes first, then the Monster.  At the conclusion of each round, the
+    user has the option of quitting.
+    ---------------------------------------------------------------------*/
+	public void battle()
+	{
+		char pause = 'p';
+		System.out.println(hero.getName() + " battles "
+				+ monster.getName()
+				+ "\n---------------------------------------------");
+
+		//do battle
+		while (hero.isAlive() && monster.isAlive() && pause != 'q')
+		{
+			//hero goes first
+			hero.battleChoices(monster);
+
+			//monster's turn (provided it's still alive!)
+			if (monster.isAlive())
+				monster.attack(hero);
+
+			//let the player bail out if desired
+			System.out.print("\n-->q to quit, anything else to continue: ");
+			Scanner kb = new Scanner(System.in);
+			pause = kb.next().charAt(0);
+
+		}//end battle loop
+
+		if (!monster.isAlive())
+			System.out.println(hero.getName() + " was victorious!");
+		else if (!hero.isAlive())
+			System.out.println(hero.getName() + " was defeated :-(");
+		else//both are alive so user quit the game
+			System.out.println("Quitters never win ;-)");
+	}//end battle method
+}
